b2ea142b3cd7f46a65ccc0ef64c23e47dfa004ad
tassaron2 <rutachigal@gmail.com>
2020-12-27 09:35:17 -0500

    added more PyPI fields to setup
    not really planning to upload this to PyPI though... maybe someday

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
 from setuptools import setup
+from os import path
+
+
+try:
+    with open(
+        path.join(path.abspath(path.dirname(__file__)), "README.md"), encoding="utf-8"
+    ) as f:
+        long_description = f.read()
+except Exception:
+    long_description = "missing README.md"
+
 
 setup(
     name="tassaron's flask template",
-    version="20.12.22",  # year.month.day
+    author="tassaron",
+    version="20.12.27",  # year.month.day
     packages=[
         "tassaron_flask_template",
         "tassaron_flask_template.main",
@@ -31,5 +43,20 @@ setup(
         "mistune==2.0.0a5",
         "python-dotenv",
         "requests",
+    ],
+    url="https://github.com/tassaron2/flask-template",
+    license="MIT",
+    description="my template for an advanced Flask app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="flask uwsgi modular template",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Framework :: Flask",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
     ],
 )
