1f747b6f6aae65387d1106d1ba223f5c72fa09fc
tassaron <rutachigal@gmail.com>
2022-02-03 13:44:38 -0500

    fix template_folder incorrect for some routes

--- tassaron_flask/flask.py
+++ tassaron_flask/flask.py
@@ -33,12 +33,16 @@ class Flask(flask.Flask):
 
     def register_modules(app, modules):
         root_blueprint, others = app.import_modules(modules)
+        root_blueprint.static_folder = "../../static"
+        root_blueprint.template_folder = f"../../templates/{root_blueprint.name}"
         app.register_blueprint(root_blueprint)
         if not root_blueprint.is_registered_index:
             raise ConfigurationError("The root blueprint failed to register. It must have the same name as its Python module.")
         for blueprint in (
             *list(others.values()),
         ):
+            blueprint.static_folder = "../../static"
+            blueprint.template_folder = f"../../templates{f'/{blueprint.name}' if blueprint.name != 'main' else ''}"
             app.register_blueprint(blueprint, url_prefix=f"/{blueprint.name}")
         for blueprint_index, tup in app.blueprint_index.items():
             endpoint, f, options = tup
