f5fde766cfc59d031e55b67b1045b549943a6f90
tassaron <rutachigal@gmail.com>
2022-02-08 23:42:20 -0500

    rename ProductPage-root to ProductPage-slideshow

--- tassaron_flask/nodejs/react/main.jsx
+++ tassaron_flask/nodejs/react/main.jsx
@@ -3,4 +3,4 @@ import ReactDOM from "react-dom";
 import ProductSlideshowFromHtml from "./ProductSlideshowFromHtml";
 import "./styles.css";
  
-ReactDOM.render(<ProductSlideshowFromHtml />, document.getElementById("ProductPage-root"));
+ReactDOM.render(<ProductSlideshowFromHtml />, document.getElementById("ProductPage-slideshow"));
