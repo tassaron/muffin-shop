6c15958bd99f119d479a37b3b21323f889cae652
tassaron <rutachigal@gmail.com>
2022-02-08 23:09:43 -0500

    stock can set to 0. DataRequired does not allow 0

--- tassaron_flask/forms/shop/inventory_forms.py
+++ tassaron_flask/forms/shop/inventory_forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField, DecimalField, IntegerField
-from wtforms.validators import DataRequired, Length, NumberRange
+from wtforms.validators import DataRequired, InputRequired, Length, NumberRange
 
 
 class ProductForm(FlaskForm):
@@ -10,7 +10,7 @@ class ProductForm(FlaskForm):
         "description", validators=[DataRequired(), Length(min=1, max=500)]
     )
     image = StringField("image", validators=[DataRequired(), Length(min=5, max=36)])
-    stock = IntegerField("stock", validators=[DataRequired(), NumberRange(min=0)])
+    stock = IntegerField("stock", validators=[InputRequired(), NumberRange(min=0)])
     category_id = IntegerField(
         "category", validators=[DataRequired(), NumberRange(min=1)]
     )
