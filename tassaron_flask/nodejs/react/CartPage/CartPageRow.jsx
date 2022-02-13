14421c73b58c03a80f1b4e6de5ed0dcba68d7f70
tassaron <rutachigal@gmail.com>
2022-02-12 21:33:28 -0500

    fit cart page buttons on a small screen

--- tassaron_flask/nodejs/react/CartPage/CartPageRow.jsx
+++ tassaron_flask/nodejs/react/CartPage/CartPageRow.jsx
@@ -5,13 +5,13 @@ class CartPageRow extends Component {
     render() {
         return (
             <div
-                className="CartPage-row row"
+                className="CartPage-row row border-top py-2"
                 data-product-id={this.props.data.id}
             >
                 <div className="CartPage-name col-12 text-center text-uppercase product-title">
                     {this.props.data.name}
                 </div>
-                <div className="CartPage-image col-5">
+                <div className="CartPage-image col-md-5 col-3">
                     <img
                         width="fit-content"
                         className="mr-2 img-fluid rounded shadow-sm"
@@ -22,7 +22,7 @@ class CartPageRow extends Component {
                 <div className="CartPage-price col-2">
                     ${this.props.data.price.toFixed(2)}
                 </div>
-                <div className="CartPage-quantity col-3">
+                <div className="CartPage-quantity col-md-3 col-5">
                     <CartPageButtons
                         quantity={this.props.data.quantity}
                         stock={this.props.data.stock}
