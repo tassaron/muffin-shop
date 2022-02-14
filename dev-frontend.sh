e5d2f67142b003eb71a33d3bf481778d1929c42a
tassaron <rutachigal@gmail.com>
2022-02-13 19:24:30 -0500

    open new window if inside a tmux session

--- dev-frontend.sh
+++ dev-frontend.sh
@@ -1,4 +1,10 @@
-tmux new-session \; \
+if [ -z "$TMUX" ]; then
+	start=new-session
+else
+	start=new-window
+fi
+
+tmux $start \; \
   send-keys './dev-backend.sh' C-m \; \
   split-window -h \; \
   send-keys 'cd tassaron_flask/nodejs && npm run watch' C-m \; 
