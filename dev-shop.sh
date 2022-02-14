eb0dcdca0377db9937c1c4f486cdd28230b36ff7
tassaron <rutachigal@gmail.com>
2022-02-14 18:35:51 -0500

    devhelp

--- dev-shop.sh
+++ dev-shop.sh
@@ -1,1 +1,1 @@
-yes | python setup/database.py test --shop; ./dev-frontend.sh
+yes | env/bin/python setup/database.py test --shop; ./dev-frontend.sh
--- tassaron_flask/nodejs/webpack.config.js
+++ tassaron_flask/nodejs/webpack.config.js
@@ -8,6 +8,7 @@ const config = {
     resolve: {
         extensions: ['.js', '.jsx', '.css']
     },
+    mode: "development",
  
     module: {
         rules: [
