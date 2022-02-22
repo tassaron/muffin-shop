90248e47316b579298afb51970bfdff41840bdcf
tassaron <rutachigal@gmail.com>
2022-02-22 09:14:56 -0500

    better error message if database can't be accessed

--- src/helpers/main/tasks.py
+++ src/helpers/main/tasks.py
@@ -4,11 +4,21 @@ Huey tasks that we can launch asynchronously from web requests
 import os
 import requests
 from huey import SqliteHuey
+from sqlite3 import OperationalError
 
 
-huey = SqliteHuey(
-    filename=os.environ.get("HUEY_DB", "db/huey.db"), immediate_use_memory=False
-)
+def init_huey():
+    huey_db_path = os.environ.get("HUEY_DB", "db/huey.db")
+    try:
+        huey = SqliteHuey(
+            filename=huey_db_path, immediate_use_memory=False
+        )
+    except OperationalError as e:
+        raise FileNotFoundError(f"{e}: {huey_db_path} does not exist. Is the directory writable?") from OperationalError
+    return huey
+
+
+huey = init_huey()
 
 
 @huey.task()
