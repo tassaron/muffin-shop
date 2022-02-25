84f36675667d2b7ca9aef316dc44e5b50ef56b70
tassaron <rutachigal@gmail.com>
2022-02-25 13:24:41 -0500

    make send_score_button a child of gamediv + a hide function

--- static/client/rainey_arcade/js/send_score.js
+++ static/client/rainey_arcade/js/send_score.js
@@ -11,9 +11,20 @@ function send_score(filename, score, token) {
     }).then(
         response => response.ok ? response.json() : null
     ).then((response) => {
-        if (!response) return;
+        if (!response || response["payout"] < 1) return;
         const tokenNode = document.getElementById("arcade_tokens");
         tokenNode.innerText = Number(tokenNode.innerText) + response["payout"];
         tokenNode.parentElement.classList.remove("d-none");
     });
+}
+
+
+function hide_send_score_button() {
+    const send_score_button = document.getElementById("send_score_button");
+    send_score_button.setAttribute("style", "display: none;");
+
+    // Remove event listeners by cloning the button
+    // sorta hacky but it's good enough for now
+    const clone = send_score_button.cloneNode(true);
+    send_score_button.parentNode.replaceChild(clone, send_score_button);
 }
