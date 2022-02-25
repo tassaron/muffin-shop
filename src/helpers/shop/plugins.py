d73143f40d4116fb9b174b01c48e465ee256e1f6
tassaron <rutachigal@gmail.com>
2022-02-25 15:31:24 -0500

    do not import stripe if stripe isn't needed

--- src/helpers/shop/plugins.py
+++ src/helpers/shop/plugins.py
@@ -1,4 +1,7 @@
 import os
-import stripe
 
-stripe.api_key = os.environ.get("STRIPE_API_KEY", "")
+payment_processor = os.environ.get("PAYMENT_PROCESSOR", "stripe")
+
+if payment_processor == "stripe":
+    import stripe
+    stripe.api_key = os.environ.get("STRIPE_API_KEY", "")
