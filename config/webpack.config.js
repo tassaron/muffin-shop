3eb36ac1525e25d2bfca79b5a0e99ccab73539eb
tassaron <rutachigal@gmail.com>
2022-02-27 18:57:01 -0500

    use production build of ReactJS when building for production

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,34 +1,42 @@
 const webpack = require("webpack");
-const config = {
-    entry: __dirname + "/../src/react/main.jsx",
-    output: {
-        path: __dirname + "/../static/js/dist",
-        filename: "bundle.js",
-    },
-    resolve: {
-        extensions: [".js", ".jsx", ".css"],
-    },
-    mode: "development",
 
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)?/,
-                exclude: /node_modules/,
-                loader: "babel-loader",
-                options: {
-                    presets: [
-                        "@babel/preset-env",
-                        ["@babel/preset-react", { runtime: "automatic" }],
-                    ],
+module.exports = (env, argv) => {
+    const config = {
+        entry: __dirname + "/../src/react/main.jsx",
+        output: {
+            path: __dirname + "/../static/js/dist",
+            filename: "bundle.js",
+        },
+        resolve: {
+            extensions: [".js", ".jsx", ".css"],
+        },
+    
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)?/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            ["@babel/preset-react", { runtime: "automatic" }],
+                        ],
+                    },
                 },
-            },
-            {
-                test: /\.css?/,
-                exclude: /node_modules/,
-                use: ["style-loader", "css-loader"],
-            },
-        ],
-    },
+                {
+                    test: /\.css?/,
+                    exclude: /node_modules/,
+                    use: ["style-loader", "css-loader"],
+                },
+            ],
+        },
+    };
+    if (argv.mode == "production") {
+        const TerserPlugin = require('terser-webpack-plugin');
+        config.optimization = {
+            minimizer: [new TerserPlugin({})],
+        }
+    }
+    return config
 };
-module.exports = config;
