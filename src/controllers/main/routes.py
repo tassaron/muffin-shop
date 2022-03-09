bbedadd2ab2d50d690c7558a925fe1c23ab32c0b
tassaron <rutachigal@gmail.com>
2022-03-08 21:40:47 -0500

    hide sessions admin page if sessions are client-side

--- src/controllers/main/routes.py
+++ src/controllers/main/routes.py
@@ -17,6 +17,7 @@ from functools import lru_cache
 from muffin_shop.blueprint import Blueprint
 from muffin_shop.helpers.main.plugins import db
 from datetime import datetime
+import os
 
 
 main_routes = Blueprint("main", __name__)
@@ -90,34 +91,35 @@ def admin_index():
     return render_template("main/admin.html", endpoints=endpoints)
 
 
-@main_routes.admin_route("/sessions")
-def admin_sessions():
-    all_sessions = current_app.session_interface.sql_session_model.query.all()
-    data = { sss.id: current_app.session_interface.decrypt(sss.data) for sss in all_sessions }
-    kv = {
-        sss.session_id: f"USER: {sss.user_id} - EXPIRY: {sss.expiry} - DATA: {data[sss.id]}" for sss in all_sessions
-    }
-    statuses = {
-        sss.session_id: f"{'expired' if sss.expiry <= datetime.utcnow() and sss.user_id is None else 'zombie' if '_user_id' in data[sss.id] and sss.user_id is None else 'deleted' if 'csrf_token' not in data[sss.id] else 'active' if ('arcade_tokens' in data[sss.id] and data[sss.id]['arcade_tokens'] > 0) or ('arcade_prizes' in data[sss.id] and data[sss.id]['arcade_prizes']) or ('cart' in data[sss.id] and data[sss.id]['cart']) else ''}" for sss in all_sessions
-    }
+if os.environ.get("CLIENT_SESSIONS", "1") == "0":
+    @main_routes.admin_route("/sessions")
+    def admin_sessions():
+        all_sessions = current_app.session_interface.sql_session_model.query.all()
+        data = { sss.id: current_app.session_interface.decrypt(sss.data) for sss in all_sessions }
+        kv = {
+            sss.session_id: f"USER: {sss.user_id} - EXPIRY: {sss.expiry} - DATA: {data[sss.id]}" for sss in all_sessions
+        }
+        statuses = {
+            sss.session_id: f"{'expired' if sss.expiry <= datetime.utcnow() and sss.user_id is None else 'zombie' if '_user_id' in data[sss.id] and sss.user_id is None else 'deleted' if 'csrf_token' not in data[sss.id] else 'active' if ('arcade_tokens' in data[sss.id] and data[sss.id]['arcade_tokens'] > 0) or ('arcade_prizes' in data[sss.id] and data[sss.id]['arcade_prizes']) or ('cart' in data[sss.id] and data[sss.id]['cart']) else ''}" for sss in all_sessions
+        }
 
-    empty_count = 0
-    zombie_count = 0
-    expired_count = 0
-    for sss in all_sessions:
-        if statuses[sss.session_id] == "deleted":
-            db.session.delete(sss)
-            empty_count += 1
-        elif statuses[sss.session_id] == "zombie":
-            db.session.delete(sss)
-            zombie_count += 1
-        elif statuses[sss.session_id] == "expired":
-            db.session.delete(sss)
-            expired_count += 1
-    if (sum([empty_count, zombie_count, expired_count]) > 0 ):
-        db.session.commit()
-        flash(f"Deleted {empty_count} empty sessions, {zombie_count} zombies, {expired_count} expired", "primary")
-    return render_template("admin/admin_kv_table.html", title="Sessions", kv=list(kv.items()), statuses=statuses)
+        empty_count = 0
+        zombie_count = 0
+        expired_count = 0
+        for sss in all_sessions:
+            if statuses[sss.session_id] == "deleted":
+                db.session.delete(sss)
+                empty_count += 1
+            elif statuses[sss.session_id] == "zombie":
+                db.session.delete(sss)
+                zombie_count += 1
+            elif statuses[sss.session_id] == "expired":
+                db.session.delete(sss)
+                expired_count += 1
+        if (sum([empty_count, zombie_count, expired_count]) > 0 ):
+            db.session.commit()
+            flash(f"Deleted {empty_count} empty sessions, {zombie_count} zombies, {expired_count} expired", "primary")
+        return render_template("admin/admin_kv_table.html", title="Sessions", kv=list(kv.items()), statuses=statuses)
 
 
 @main_routes.app_errorhandler(BadRequest)
