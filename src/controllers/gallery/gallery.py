f19205590dec0726695c2bcd26a6be37a94fd980
tassaron <rutachigal@gmail.com>
2022-03-08 21:21:10 -0500

    gallery takes posts from blog

--- src/controllers/gallery/gallery.py
+++ src/controllers/gallery/gallery.py
@@ -1,7 +1,13 @@
+"""
+Gallery module shows all images uploaded to the site + the newest blog posts
+Currently it depends on the blog module existing but that could change in future
+"""
 from flask import render_template, url_for
 from muffin_shop.blueprint import Blueprint
 from muffin_shop.helpers.main.markdown import render_markdown
 from muffin_shop.helpers.main.images import get_files
+import json
+import os
 
 
 blueprint = Blueprint(
@@ -12,9 +18,12 @@ blueprint = Blueprint(
 
 @blueprint.index_route()
 def gallery_index():
+    with open(f"{os.environ['BLOG_PATH']}/posts.json", "r") as f:
+        posts = json.load(f)
     return render_template(
         "gallery/gallery_index.html",
         images=[url_for('static', filename=f'uploads/images/{image}') for image in get_files()],
         header=render_markdown("gallery/header.md"),
-        footer=render_markdown("gallery/footer.md")
+        footer=render_markdown("gallery/footer.md"),
+        posts=reversed(posts),
     )
